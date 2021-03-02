077f27435c389fb401f5a9940c1ac251eb6a1958
lukeshingles <luke.shingles@gmail.com>
2021-03-02 12:55:12 +0000

    Fix hatches circular import

--- artistools/spectra/__init__.py
+++ artistools/spectra/__init__.py
@@ -2,7 +2,6 @@
 """Artistools - spectra related functions."""
 
 # import artistools as at
-import artistools.spectra.data
 
 from artistools.spectra.plot import main, addargs
 from artistools.spectra.data import *
