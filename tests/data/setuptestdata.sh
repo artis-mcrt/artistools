96639ca37f1152031d0cc53165ebca0339be7a09
lukeshingles <luke.shingles@gmail.com>
2021-03-23 16:19:50 +0000

    Add setuptestdata.sh script

---
+++ tests/data/setuptestdata.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env zsh -x
+
+if [ ! -f testmodel.tar.xz ]; then curl -O https://psweb.mp.qub.ac.uk/artis/artistools/testmodel.tar.xz; fi
+
+mkdir -p artismodel/
+tar -xf testmodel.tar.xz --directory artismodel/
