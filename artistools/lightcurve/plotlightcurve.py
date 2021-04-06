00a0c4de90657f8d1279810b9013f33ec5ac41f9
Christine <ccollins22@qub.ac.uk>
2021-04-06 15:37:56 +0200

    Move to function

--- artistools/lightcurve/plotlightcurve.py
+++ artistools/lightcurve/plotlightcurve.py
@@ -655,6 +655,26 @@ def make_viewing_angle_peakmag_delta_m15_scatter_plot(modelnames, key, args):
     plt.close()
 
 
+def get_colour_delta_mag(band_lightcurve_data, filter_names):
+    time_dict_1 = {}
+    time_dict_2 = {}
+
+    plot_times = []
+    colour_delta_mag = []
+
+    for filter_1, filter_2 in zip(band_lightcurve_data[filter_names[0]], band_lightcurve_data[filter_names[1]]):
+        # Make magnitude dictionaries where time is the key
+        time_dict_1[float(filter_1[0])] = filter_1[1]
+        time_dict_2[float(filter_2[0])] = filter_2[1]
+
+    for time in time_dict_1.keys():
+        if time in time_dict_2.keys():  # Test if time has a magnitude for both filters
+            plot_times.append(time)
+            colour_delta_mag.append(time_dict_1[time] - time_dict_2[time])
+
+    return plot_times, colour_delta_mag
+
+
 def colour_evolution_plot(modelpaths, filternames_conversion_dict, outputfolder, args):
     args.labelfontsize = 24  #todo: make command line arg
     angle_counter = 0
@@ -674,21 +694,7 @@ def colour_evolution_plot(modelpaths, filternames_conversion_dict, outputfolder,
                 args.filter = filter_names
                 band_lightcurve_data = get_band_lightcurve_data(modelpath, args, angle=angle, modelnumber=modelnumber)
 
-                time_dict_1 = {}
-                time_dict_2 = {}
-
-                plot_times = []
-                colour_delta_mag = []
-
-                for filter_1, filter_2 in zip(band_lightcurve_data[filter_names[0]], band_lightcurve_data[filter_names[1]]):
-                    # Make magnitude dictionaries where time is the key
-                    time_dict_1[float(filter_1[0])] = filter_1[1]
-                    time_dict_2[float(filter_2[0])] = filter_2[1]
-
-                for time in time_dict_1.keys():
-                    if time in time_dict_2.keys():  # Test if time has a magnitude for both filters
-                        plot_times.append(time)
-                        colour_delta_mag.append(time_dict_1[time] - time_dict_2[time])
+                plot_times, colour_delta_mag = get_colour_delta_mag(band_lightcurve_data, filter_names)
 
                 linelabel = get_linelabel(modelpath, modelname, modelnumber, angle, angle_definition, args)
 
