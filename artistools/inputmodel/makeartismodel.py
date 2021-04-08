650bf35468a75e7c6b164069aca45a2054004726
Christine <ccollins22@qub.ac.uk>
2021-04-08 20:04:31 +0200

    Make energy input files

--- artistools/inputmodel/makeartismodel.py
+++ artistools/inputmodel/makeartismodel.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import argparse
 import artistools as at
 import artistools.inputmodel.downscale3dgrid
+import artistools.inputmodel.makeenergyinputfiles
 
 
 def addargs(parser):
@@ -10,6 +11,12 @@ def addargs(parser):
 
     parser.add_argument('--downscale3dgrid', action='store_true',
                         help='Downscale a 3D ARTIS model to smaller grid size')
+
+    parser.add_argument('--makeenergyinputfiles', action='store_true',
+                        help='Downscale a 3D ARTIS model to smaller grid size')
+
+    parser.add_argument('-modeldim', type=int, default=None,
+                        help='Choose how many dimensions input model has. 1, 2 or 3')
 
 
 def main(args=None, argsraw=None, **kwargs):
@@ -31,6 +38,21 @@ def main(args=None, argsraw=None, **kwargs):
 
     if args.downscale3dgrid:
         at.inputmodel.downscale3dgrid.make_downscaled_3d_grid(modelpath=Path(args.modelpath[0]))
+        return
+
+    if args.makeenergyinputfiles:
+        model, t_model, vmax = at.inputmodel.get_modeldata(args.modelpath[0])
+        if args.modeldim == 1:
+            rho = 10**model['logrho']
+
+        else:
+            rho = model['rho']
+
+        Mtot_grams = model['shellmass_grams'].sum()
+        print(f"total mass { Mtot_grams / 1.989e33} Msun")
+
+        at.inputmodel.makeenergyinputfiles.make_energy_files(rho, Mtot_grams)
+        return
 
 
 if __name__ == '__main__':
