0d188555eb5fea68141ab9998ca52a91c9f768d4
Luke Shingles <luke.shingles@gmail.com>
2022-03-17 16:06:42 +0000

    Update describeinputmodel.py

--- artistools/inputmodel/describeinputmodel.py
+++ artistools/inputmodel/describeinputmodel.py
@@ -38,14 +38,15 @@ def main(args=None, argsraw=None, **kwargs):
     print(f'Model is defined at {t_model_init_days} days ({t_model_init_seconds:.4f} seconds)')
 
     if 'pos_x' in dfmodel.columns:
-        print(f'Model contains {len(dfmodel)} Cartesian grid cells with vmax = {vmax} km/s')
+        nonemptycells = sum(dfmodel['rho'] > 0.)
+        print(f'Model contains {len(dfmodel)} Cartesian grid cells ({nonemptycells} nonempty) with vmax = {vmax} km/s')
     else:
         vmax = dfmodel['velocity_outer'].max()
         print(f'Model contains {len(dfmodel)} 1D spherical shells with vmax = {vmax} km/s')
 
     mass_msun_rho = dfmodel['cellmass_grams'].sum() / 1.989e33
 
-    mass_msun_radioisotopes = 0.
+    mass_msun_isotopes = 0.
     mass_msun_elem = 0.
     speciesmasses = {}
     for column in dfmodel.columns:
@@ -55,18 +56,18 @@ def main(args=None, argsraw=None, **kwargs):
 
             mass_msun = speciesabund_g / 1.989e33
             if species[-1].isdigit():
-                mass_msun_radioisotopes += mass_msun
+                mass_msun_isotopes += mass_msun
             elif species.lower() != 'fegroup':
                 mass_msun_elem += mass_msun
 
             if speciesabund_g > 0.:
                 speciesmasses[species] = speciesabund_g
 
-    print(f'M_{"tot_rho":8s} {mass_msun_rho:7.4f} MSun (from rho)')
+    print(f'M_{"tot_rho":8s} {mass_msun_rho:7.4f} MSun (density * volume)')
     if mass_msun_elem > 0.:
-        print(f'M_{"tot_elem":8s} {mass_msun_elem:7.4f} MSun')
+        print(f'M_{"tot_elem":8s} {mass_msun_elem:7.4f} MSun ({mass_msun_elem / mass_msun_rho * 100:6.2f}% of M_tot_rho)')
 
-    print(f'M_{"tot_iso":8s} {mass_msun_radioisotopes:7.4f} MSun (usually just radioactive isotopes, so can be much less than Mtot)')
+    print(f'M_{"tot_iso":8s} {mass_msun_isotopes:7.4f} MSun ({mass_msun_isotopes / mass_msun_rho * 100:6.2f}% of M_tot_rho, but can be small if stable isotopes not tracked)')
 
     for species, mass_g in speciesmasses.items():
         mass_msun = mass_g / 1.989e33
