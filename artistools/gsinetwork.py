a37382176ce9cc8fa82f2d652619bfc6cb7527b3
Luke Shingles <luke.shingles@gmail.com>
2022-03-23 09:51:33 +0000

    Add particle-weighted abundance and heating comparisons to GSI network

--- artistools/gsinetwork.py
+++ artistools/gsinetwork.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
+# PYTHON_ARGCOMPLETE_OK
 
-# import numpy as np
+import argcomplete
+import argparse
+
+import numpy as np
 # import io
 import pandas as pd
 # import math
@@ -13,26 +17,25 @@ import artistools as at
 # import artistools.estimators
 
 
-def plot_qdot(modelpath, dfthermo, dfheating):
+def plot_qdot(modelpath, particledata, arr_time_s):
     depdata = at.get_deposition(modelpath=modelpath)
 
-    df = pd.merge(dfthermo, dfheating, how='inner', on=['#count', 'time/s', 'rho(g/cc)'])
-    pd.options.display.max_rows = 50
-    pd.options.display.width = 250
-    pd.set_option("display.width", 250)
     tstart = depdata['tmid_days'].min()
     tend = depdata['tmid_days'].max()
-    df.eval("time_d = `time/s`  / 86400", inplace=True)
-    df.query("`time_d` >= @tstart - 0.3 and `time_d` <= @tend + 1.5", inplace=True)
 
-    df['hspof'] = df['hspof'].astype(float)
-    df['hbfis'] = df['hbfis'].astype(float)
+    arr_heat = {}
+    frac_of_cellmass_sum = sum([p['frac_of_cellmass'] for _, p in particledata.items()])
+    heatcols = ['hbeta', 'halpha', 'hbfis', 'hspof']
+    # heatcols = ['hbeta', 'halpha']
+    for col in heatcols:
+        arr_heat[col] = np.zeros_like(arr_time_s)
 
-    print(depdata.columns)
-    print(df.columns)
-    print(df)
-
-    arr_time_s, arr_qdot = dfthermo['time/s'], dfthermo['Qdot']
+    for particleid, thisparticledata in particledata.items():
+        for col in heatcols:
+            arr_time_s_source = sorted(list(thisparticledata[col].keys()))
+            arr_heat_source = np.array([thisparticledata[col][t] for t in arr_time_s_source])
+            heatrates_interp = np.interp(arr_time_s, arr_time_s_source, arr_heat_source)
+            arr_heat[col] += heatrates_interp * thisparticledata['frac_of_cellmass'] / frac_of_cellmass_sum
 
     fig, axis = plt.subplots(
         nrows=1, ncols=1, sharex=False, sharey=False, figsize=(6, 4),
@@ -49,9 +52,9 @@ def plot_qdot(modelpath, dfthermo, dfheating):
     # axis.set_ylabel(f'X({strnuc})')
     axis.set_ylabel('Qdot [erg/g/s]')
     arr_time_days = arr_time_s / 86400
-    arr_time_days, arr_qdot = zip(
-        *[(t, qdot) for t, qdot in zip(arr_time_days, arr_qdot)
-          if depdata['tmid_days'].min() <= t and t <= depdata['tmid_days'].max()])
+    # arr_time_days, arr_qdot = zip(
+    #     *[(t, qdot) for t, qdot in zip(arr_time_days, arr_qdot)
+    #       if depdata['tmid_days'].min() <= t and t <= depdata['tmid_days'].max()])
 
     # axis.plot(df['time_d'], df['Qdot'],
     #           # linestyle='None',
@@ -65,7 +68,7 @@ def plot_qdot(modelpath, dfthermo, dfheating):
     #           # marker='+', markersize=15,
     #           label='Qdot ARTIS')
 
-    axis.plot(df['time_d'], df['hbeta'],
+    axis.plot(arr_time_days, arr_heat['hbeta'],
               linewidth=2, color='black',
               linestyle='dashed',
               # marker='x', markersize=8,
@@ -77,7 +80,7 @@ def plot_qdot(modelpath, dfthermo, dfheating):
               # marker='+', markersize=15,
               label=r'$\dot{Q}_\beta$ ARTIS')
 
-    axis.plot(df['time_d'], df['halpha'],
+    axis.plot(arr_time_days, arr_heat['halpha'],
               linewidth=2, color='black',
               linestyle='dotted',
               # marker='x', markersize=8,
@@ -89,14 +92,14 @@ def plot_qdot(modelpath, dfthermo, dfheating):
               # marker='+', markersize=15,
               label=r'$\dot{Q}_\alpha$ ARTIS')
 
-    axis.plot(df['time_d'], df['hbfis'],
+    axis.plot(arr_time_days, arr_heat['hbfis'],
               linewidth=2,
               linestyle='dotted',
               # marker='x', markersize=8,
               # color='black',
               label=r'$\dot{Q}_{hbfis}$ GSI Network')
 
-    axis.plot(df['time_d'], df['hspof'],
+    axis.plot(arr_time_days, arr_heat['hspof'],
               linewidth=2,
               linestyle='dotted',
               # marker='x', markersize=8,
@@ -109,7 +112,30 @@ def plot_qdot(modelpath, dfthermo, dfheating):
     plt.savefig(Path(modelpath, 'gsinetwork-qdot.pdf'), format='pdf')
 
 
-def main():
+def addargs(parser):
+    parser.add_argument('-modelpath',
+                        default='.',
+                        help='Path for ARTIS files')
+
+    parser.add_argument('-outputpath', '-o',
+                        default='.',
+                        help='Path for output files')
+
+    parser.add_argument('-modelgridindex', '-cell', '-mgi', type=int, default=0,
+                        help='Modelgridindex to plot')
+
+
+def main(args=None, argsraw=None, **kwargs):
+    if args is None:
+        parser = argparse.ArgumentParser(
+            formatter_class=at.CustomArgHelpFormatter,
+            description='Create solar r-process pattern in ARTIS format.')
+
+        addargs(parser)
+        parser.set_defaults(**kwargs)
+        argcomplete.autocomplete(parser)
+        args = parser.parse_args(argsraw)
+
     arr_el_a = [
         ('Ga', 72),
         ('Sr', 89),
@@ -131,9 +157,8 @@ def main():
 
     arr_z = [at.elsymbols.index(el) for el in arr_el]
 
-    rundir = Path('/Users/luke/Library/Mobile Documents/com~apple~CloudDocs/artis_runs')
-    # modelpath = Path(rundir, 'kilonova_rprocesstraj_testalphameantomweight')
-    modelpath = Path(rundir, 'kilonova_rprocesstraj_testalphameanatomweight_redoqdot')
+    modelpath = Path(args.modelpath)
+    mgi = int(args.modelgridindex)
 
     dfmodel, t_model_init_days, vmax_cmps = at.inputmodel.get_modeldata(modelpath)
     rho_init_cgs = 10 ** dfmodel.iloc[0].logrho
@@ -142,15 +167,17 @@ def main():
     arr_time_artis = []
     arr_abund_artis = {}
 
-    estimators = at.estimators.read_estimators(modelpath)
-    for ts_mgi in estimators.keys():
-        # print(estimators[ts_mgi])
-        time_days = float(estimators[ts_mgi]['tdays'])
+    estimators = at.estimators.read_estimators(modelpath, modelgridindex=mgi)
+    for key_ts, key_mgi in estimators.keys():
+        if key_mgi != mgi:
+            continue
+
+        time_days = float(estimators[(key_ts, key_mgi)]['tdays'])
         arr_time_artis.append(time_days)
 
         rho_cgs = rho_init_cgs * (t_model_init_days / time_days) ** 3
         for strnuc, a in zip(arr_strnuc, arr_a):
-            abund = estimators[ts_mgi]['populations'][strnuc]
+            abund = estimators[(key_ts, key_mgi)]['populations'][strnuc]
             massfrac = abund * a * MH / rho_cgs
             if strnuc not in arr_abund_artis:
                 arr_abund_artis[strnuc] = []
@@ -160,47 +187,83 @@ def main():
     arr_abund_gsi = {}
 
     print('Reading trajectory data')
-    trajtar = Path('/Volumes/GoogleDrive/My Drive/Archive/Astronomy Downloads/Mergers/SFHo/88969.tar.xz')
-    tar = tarfile.open(trajtar, mode='r:*')
+    dfpartcontrib = at.inputmodel.rprocess_from_trajectory.get_gridparticlecontributions(modelpath)
+    dfpartcontrib.query('cellindex == @mgi + 1', inplace=True)
 
-    # files in the compressed tarfile should be read in the natural order because seeking is extremely slow
-    nzplanedata = {}
-    for member in tar.getmembers():
-        if member.isfile:
-            if member.name.startswith('./Run_rprocess/nz-plane'):
-                with tar.extractfile(member) as f:
-                    header_str = f.readline().split()
-                    df = pd.read_csv(f, delim_whitespace=True, names=['N', 'Z', 'log10abund', 'S1n', 'S2n'])
-                    nzplanedata[member.name] = header_str, df
-            elif member.name == './Run_rprocess/energy_thermo.dat':
-                with tar.extractfile(member) as f:
-                    dfthermo = pd.read_csv(f, delim_whitespace=True)
-            elif member.name == './Run_rprocess/heating.dat':
-                with tar.extractfile(member) as f:
-                    dfheating = pd.read_csv(f, delim_whitespace=True)
+    particledata = {}
+    for particleid, frac_of_cellmass in dfpartcontrib[['particleid', 'frac_of_cellmass']].itertuples(index=False):
+        trajtar = at.inputmodel.rprocess_from_trajectory.get_traj_tarpath(particleid)
+        try:
+            with tarfile.open(trajtar, mode='r:*') as tar:
+                particledata[particleid] = {
+                    'frac_of_cellmass': frac_of_cellmass,
+                    'Qdot': {},
+                    'hbeta': {},
+                    'halpha': {},
+                    'hbfis': {},
+                    'hspof': {},
+                    **{strnuc: {} for strnuc in arr_strnuc}
+                }
+                # for strnuc in arr_strnuc:
+                #     particledata[particleid][strnuc] = {}
 
-    plot_qdot(modelpath, dfthermo, dfheating)
+                # files in the compressed tarfile should be read in the natural order because seeking is extremely slow
+                for member in tar.getmembers():
+                    if member.isfile:
+                        if member.name.startswith('./Run_rprocess/nz-plane'):
+                            with tar.extractfile(member) as f:
+                                header_str = f.readline().split()
+                                header = int(header_str[0]), *[float(x) for x in header_str[1:]]
+                                filecount, time, T9, rho, neutron_number_density, trajectory_mass = header
 
-    for member in sorted(nzplanedata.keys()):
-        header_str, df = nzplanedata[member]
-        header = int(header_str[0]), *[float(x) for x in header_str[1:]]
-        filecount, time, T9, rho, neutron_number_density, trajectory_mass = header
-        # print(header)
-        time_days = time / 8.640000E+04
-        if not (arr_time_artis[0] <= time_days <= arr_time_artis[-1]):
+                                dfabund = pd.read_csv(
+                                    f, delim_whitespace=True,
+                                    names=['N', 'Z', 'log10abund', 'S1n', 'S2n'], usecols=['N', 'Z', 'log10abund'])
+
+                                dfabund.eval('massfrac = (N + Z) * (10 ** log10abund)', inplace=True)
+
+                                for strnuc, a, z in zip(arr_strnuc, arr_a, arr_z):
+                                    particledata[particleid][strnuc][time] = (
+                                        dfabund.query('(N + Z) == @a and Z == @z')['massfrac'].sum())
+
+                        # elif member.name == './Run_rprocess/energy_thermo.dat':
+                        #     with tar.extractfile(member) as f:
+                        #         dfthermo = pd.read_csv(f, delim_whitespace=True, usecols=['#count', 'time/s', 'Qdot'])
+                        #         print(dfthermo)
+                        elif member.name == './Run_rprocess/heating.dat':
+                            with tar.extractfile(member) as f:
+                                dfheating = pd.read_csv(
+                                    f, delim_whitespace=True, usecols=[
+                                        '#count', 'time/s', 'hbeta', 'halpha', 'hbfis', 'hspof'])
+                                heatcols = ['hbeta', 'halpha', 'hbfis', 'hspof']
+                                for _, row in dfheating.iterrows():
+                                    time_s = row['time/s']
+                                    for col in heatcols:
+                                        try:
+                                            particledata[particleid][col][time_s] = float(row[col])
+                                        except ValueError:
+                                            particledata[particleid][col][time_s] = float(row[col].replace('-', 'e-'))
+
+
+        except FileNotFoundError:
+            print(f'WARNING: Particle data not found for id {particleid}')
             continue
-        arr_time_gsi.append(time_days)
 
-        df.eval('abund = 10 ** log10abund', inplace=True)
-        df.eval('A = N + Z', inplace=True)
-        df.eval('massfrac = A * abund', inplace=True)
+    frac_of_cellmass_sum = sum([p['frac_of_cellmass'] for _, p in particledata.items()])
 
+    arr_time_artis_s = np.array([t * 8.640000E+04 for t in arr_time_artis])
+    for strnuc in arr_strnuc:
+        arr_abund_gsi[strnuc] = np.zeros_like(arr_time_artis_s)
+
+    for particleid, thisparticledata in particledata.items():
         for strnuc, a, z in zip(arr_strnuc, arr_a, arr_z):
-            if strnuc not in arr_abund_gsi:
-                arr_abund_gsi[strnuc] = []
+            arr_time_s = sorted(list(thisparticledata[strnuc].keys()))
+            # arr_time_s_float = [float(t) for t in arr_time_s]
+            arr_massfracs = np.array([thisparticledata[strnuc][t] for t in arr_time_s])
+            massfracs_interp = np.interp(arr_time_artis_s, arr_time_s, arr_massfracs)
+            arr_abund_gsi[strnuc] += massfracs_interp * thisparticledata['frac_of_cellmass'] / frac_of_cellmass_sum
 
-            massfrac = df.query('A == @a and Z == @z')['massfrac'].sum()
-            arr_abund_gsi[strnuc].append(massfrac)
+    plot_qdot(modelpath, particledata, arr_time_artis_s)
 
     fig, axes = plt.subplots(
         nrows=len(arr_strnuc), ncols=1, sharex=False, sharey=False, figsize=(6, 1 + 2. * len(arr_strnuc)),
@@ -215,7 +278,7 @@ def main():
         # axis.set_ylabel(f'X({strnuc})')
         axis.set_ylabel('Mass fraction')
 
-        axis.plot(arr_time_gsi, arr_abund_gsi[strnuc],
+        axis.plot(arr_time_artis, arr_abund_gsi[strnuc],
                   # linestyle='None',
                   linewidth=2,
                   marker='x', markersize=8,
@@ -227,10 +290,11 @@ def main():
                   # marker='+', markersize=15,
                   label=f'{strnuc} ARTIS', color='red')
 
-        axis.legend(loc='best', frameon=False, handlelength=1, ncol=1,
-                    numpoints=1)
+        axis.legend(loc='best', frameon=False, handlelength=1, ncol=1, numpoints=1)
 
-    plt.savefig(Path(modelpath, 'gsinetwork-abundance.pdf'), format='pdf')
+    pdfoutpath = Path(modelpath, 'gsinetwork-abundance.pdf')
+    plt.savefig(pdfoutpath, format='pdf')
+    print(f'Saved {pdfoutpath}')
 
 
 if __name__ == "__main__":
