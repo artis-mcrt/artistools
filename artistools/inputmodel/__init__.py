Fix gridcontributions.txt for 1D spherical average

artistools/inputmodel/__init__.py
@@ -373,8 +373,9 @@ def save_modeldata(
         print(f' grid size: {len(dfmodel)} ({griddimension}^3)')
         assert griddimension ** 3 == len(dfmodel)
 
-        standardcols = ['inputcellid', 'pos_x_min', 'pos_y_min', 'pos_z_min', 'rho',  'X_Fegroup', 'X_Ni56', 'X_Co56', 'X_Fe52',
-                        'X_Cr48']
+        standardcols = [
+            'inputcellid', 'pos_x_min', 'pos_y_min', 'pos_z_min', 'rho',
+            'X_Fegroup', 'X_Ni56', 'X_Co56', 'X_Fe52', 'X_Cr48']
 
     # these two columns are optional, but position is important and they must appear before any other custom cols
     if 'X_Ni57' in dfmodel.columns:
@@ -541,11 +542,16 @@ def sphericalaverage(dfmodel, t_model_init_days, vmax, dfelabundances=None, dfgr
     ncoordgridx = round(ngridpoints ** (1. / 3.))
     wid_init = 2 * xmax / ncoordgridx
 
-    print(f'Spherically averaging 3D model with {ngridpoints} cells...')
+    print(f'Spherically averaging 3D model with {ngridpoints} cells...', end='')
+    timestart = time.perf_counter()
 
+    cell_frac_sum = {}
+    for cellindex, dfcellcontribs in dfgridcontributions.groupby('cellindex'):
+        cell_frac_sum[cellindex] = dfcellcontribs.frac_of_cellmass.sum()
+
+    # dfmodel = dfmodel.query('rho > 0.').copy()
     dfmodel = dfmodel.copy()
-    if dfelabundances is not None:
-        dfelabundances = dfelabundances.query('inputcellid in @dfmodel.inputcellid').copy()
+
     dfmodel = add_derived_cols_to_modeldata(
         dfmodel, ['velocity'], dimensions=3, t_model_init_seconds=t_model_init_seconds, wid_init=wid_init)
     # print(dfmodel)
@@ -554,21 +560,39 @@ def sphericalaverage(dfmodel, t_model_init_days, vmax, dfelabundances=None, dfgr
     velocity_bins = [vmax * n / ncoordgridx for n in range(ncoordgridx + 1)]  # cm/s
     outcells = []
     outcellabundances = []
-    # binned = pd.cut(dfmodel['vel_mid_radial'], velocity_bins, labels=False, include_lowest=True)
-    # for radialcellid, matchedcells in dfmodel.groupby(binned):
-    cellid_3d_to_1d_map = {}
+    outgridcontributions = []
+
+    # cellidmap_3d_to_1d = {}
     highest_active_radialcellid = -1
     for radialcellid, (velocity_inner, velocity_outer) in enumerate(zip(velocity_bins[:-1], velocity_bins[1:]), 1):
         assert velocity_outer > velocity_inner
-        matchedcells = dfmodel.query('vel_mid_radial > @velocity_inner and vel_mid_radial <= @velocity_outer')
-        cellid_3d_to_1d_map.update({cellid_3d: radialcellid for cellid_3d in matchedcells.inputcellid})
+        matchedcells = dfmodel.query(
+            'rho > 0. and vel_mid_radial > @velocity_inner and vel_mid_radial <= @velocity_outer')
+        matchedcellrhosum = matchedcells.rho.sum()
+        # cellidmap_3d_to_1d.update({cellid_3d: radialcellid for cellid_3d in matchedcells.inputcellid})
 
         if len(matchedcells) == 0:
             rhomean = 0.
         else:
             shell_volume = (4 * math.pi / 3) * (
                 (velocity_outer * t_model_init_seconds) ** 3 - (velocity_inner * t_model_init_seconds) ** 3)
-            rhomean = matchedcells.rho.sum() * wid_init ** 3 / shell_volume
+            rhomean = matchedcellrhosum * wid_init ** 3 / shell_volume
+
+            if rhomean > 0. and dfgridcontributions is not None:
+                dfcellcont = dfgridcontributions.query('cellindex in @matchedcells.inputcellid.values')
+
+                for particleid, dfparticlecontribs in dfcellcont.groupby('particleid'):
+                    frac_of_cellmass_avg = 0.
+
+                    for inputcellid, rho in matchedcells[['inputcellid', 'rho']].itertuples(index=False):
+                        contrib = dfparticlecontribs.query('cellindex == @inputcellid').frac_of_cellmass.sum()
+                        frac_of_cellmass_avg += contrib / cell_frac_sum[inputcellid] * rho / matchedcellrhosum
+
+                    outgridcontributions.append({
+                        'particleid': particleid,
+                        'cellindex': radialcellid,
+                        'frac_of_cellmass': frac_of_cellmass_avg,
+                    })
 
         if rhomean > 0.:
             highest_active_radialcellid = radialcellid
@@ -582,7 +606,7 @@ def sphericalaverage(dfmodel, t_model_init_days, vmax, dfelabundances=None, dfgr
         for column in matchedcells.columns:
             if column.startswith('X_'):
                 if rhomean > 0.:
-                    massfrac = np.dot(matchedcells[column], matchedcells['rho']) / matchedcells['rho'].sum()
+                    massfrac = np.dot(matchedcells[column], matchedcells.rho) / matchedcellrhosum
                 else:
                     massfrac = 0.
                 dictcell[column] = massfrac
@@ -597,7 +621,7 @@ def sphericalaverage(dfmodel, t_model_init_days, vmax, dfelabundances=None, dfgr
             for column in dfelabundances.columns:
                 if column.startswith('X_'):
                     if rhomean > 0.:
-                        massfrac = np.dot(abund_matchedcells[column], matchedcells['rho']) / matchedcells['rho'].sum()
+                        massfrac = np.dot(abund_matchedcells[column], matchedcells.rho) / matchedcellrhosum
                     else:
                         massfrac = 0.
                     dictcellabundances[column] = massfrac
@@ -605,14 +629,11 @@ def sphericalaverage(dfmodel, t_model_init_days, vmax, dfelabundances=None, dfgr
             outcellabundances.append(dictcellabundances)
 
     dfmodel1d = pd.DataFrame(outcells[:highest_active_radialcellid])
-    dfabundances1d = pd.DataFrame(outcellabundances[:highest_active_radialcellid]) if outcellabundances else None
 
-    dfgridcontributions1d = dfgridcontributions.query('cellindex in @cellid_3d_to_1d_map').copy()
-    dfgridcontributions1d.rename(columns={"cellindex": "cellindex_3d"}, inplace=True)
-    dfgridcontributions1d['cellindex'] = dfgridcontributions1d['cellindex_3d'].map(cellid_3d_to_1d_map).astype(int)
-    dfgridcontributions1d.drop(columns='cellindex_3d', inplace=True)
+    dfabundances1d = (
+        pd.DataFrame(outcellabundances[:highest_active_radialcellid]) if outcellabundances else None)
 
-    # dfgridcontributions1d['cellindex'] = dfgridcontributions1d['cellindex'].astype(int)
-    dfgridcontributions1d = dfgridcontributions1d.groupby(['particleid', 'cellindex'], as_index=False).sum()
+    dfgridcontributions1d = pd.DataFrame(outgridcontributions) if outgridcontributions else None
+    print(f'took {time.perf_counter() - timestart:.1f} seconds')
 
     return dfmodel1d, dfabundances1d, dfgridcontributions1d

artistools/inputmodel/rprocess_from_trajectory.py
@@ -45,9 +45,21 @@ def get_dfevol(particleid):
     return dfevol
 
 
-def get_closest_network_timestep(particleid, t_model_s):
+def get_closest_network_timestep(particleid, timesec, cond='nearest'):
+    """
+        cond:
+            'lessthan': find highest timestep less than time_sec
+            'greaterthan': find lowest timestep greater than time_sec
+    """
     dfevol = get_dfevol(particleid)
-    idx = np.abs(dfevol.timesec.values - t_model_s).argmin()
+    if cond == 'nearest':
+        idx = np.abs(dfevol.timesec.values - timesec).argmin()
+    elif cond == 'greaterthan':
+        return dfevol.query('timesec > @timesec').nstep.min()
+    elif cond == 'lessthan':
+        return dfevol.query('timesec < @timesec').nstep.max()
+    else:
+        assert False
     nts = dfevol.nstep.values[idx]
 
     return nts
@@ -76,7 +88,10 @@ def get_trajectory_timestepfile_nuc_abund(particleid, memberfilename):
     return dfnucabund, t_model_init_seconds
 
 
-def get_trajectory_time_nuc_abund(particleid, t_model_s=None, nts=None):
+def get_trajectory_nuc_abund(particleid, t_model_s=None, nts=None):
+    """
+        nts: GSI network timestep number
+    """
     assert t_model_s is not None or nts is not None
     try:
         if nts is not None:
@@ -107,7 +122,8 @@ def get_trajectory_time_nuc_abund(particleid, t_model_s=None, nts=None):
     # print(f'trajectory particle id {particleid} massfrac sum: {massfractotal:.2f}')
     # print(f' grid snapshot: {t_model_s:.2e} s, network: {traj_time_s:.2e} s (timestep {nts})')
     assert np.isclose(massfractotal, 1., rtol=0.02)
-    assert np.isclose(traj_time_s, t_model_s, rtol=0.2, atol=1.)
+    if nts is None:
+        assert np.isclose(traj_time_s, t_model_s, rtol=0.2, atol=1.)
 
     dict_traj_nuc_abund = {nucabundcolname: massfrac / massfractotal for nucabundcolname, massfrac in colmassfracs}
     return dict_traj_nuc_abund
@@ -156,6 +172,32 @@ def get_gridparticlecontributions(gridcontribpath):
     return dfcontribs
 
 
+def filtermissinggridparticlecontributions(dfcontribs):
+    missing_particleids = []
+    for particleid in sorted(dfcontribs.particleid.unique()):
+        if not get_traj_tarpath(particleid).is_file():
+            missing_particleids.append(particleid)
+            # print(f' WARNING particle {particleid} not found!')
+
+    print(f'Removing grid contributions from {len(missing_particleids)} '
+          'particles with no abundance data and renormalising...', end='')
+    dfcontribs.query('particleid not in @missing_particleids', inplace=True)
+
+    cell_frac_sum = {}
+    for cellindex, dfparticlecontribs in dfcontribs.groupby('cellindex'):
+        cell_frac_sum[cellindex] = dfparticlecontribs.frac_of_cellmass.sum()
+
+    dfcontribs['frac_of_cellmass'] = [
+        row.frac_of_cellmass / cell_frac_sum[row.cellindex] for row in dfcontribs.itertuples()]
+
+    for cellindex, dfparticlecontribs in dfcontribs.groupby('cellindex'):
+        frac_sum = dfparticlecontribs.frac_of_cellmass.sum()
+        assert np.isclose(frac_sum, 1., rtol=0.02)
+    print('done')
+
+    return dfcontribs
+
+
 def save_gridparticlecontributions(dfcontribs, gridcontribpath):
     gridcontribpath = Path(gridcontribpath)
     if gridcontribpath.is_dir():
@@ -163,20 +205,23 @@ def save_gridparticlecontributions(dfcontribs, gridcontribpath):
     dfcontribs.to_csv(gridcontribpath, sep=' ', index=False)
 
 
-def add_abundancecontributions(gridcontribpath, dfmodel, t_model_days, minparticlespercell=0):
+def add_abundancecontributions(dfgridcontributions, dfmodel, t_model_days, minparticlespercell=0):
     """ contribute trajectory network calculation abundances to model cell abundances """
     t_model_s = t_model_days * 86400
+    dfcontribs = dfgridcontributions
+
     if 'X_Fegroup' not in dfmodel.columns:
         dfmodel = pd.concat([dfmodel, pd.DataFrame({'X_Fegroup': np.ones(len(dfmodel))})], axis=1)
-
-    dfcontribs = get_gridparticlecontributions(gridcontribpath)
 
     active_inputcellids = [
         cellindex for cellindex, dfthiscellcontribs in dfcontribs.groupby('cellindex')
         if len(dfthiscellcontribs) >= minparticlespercell]
+
     dfcontribs.query('cellindex in @active_inputcellids', inplace=True)
+    # dfcontribs = filtermissinggridparticlecontributions(dfcontribs)
     active_inputcellids = dfcontribs.cellindex.unique()
     active_inputcellcount = len(active_inputcellids)
+
     dfcontribs_particlegroups = dfcontribs.groupby('particleid')
     particle_count = len(dfcontribs_particlegroups)
 
@@ -186,7 +231,7 @@ def add_abundancecontributions(gridcontribpath, dfmodel, t_model_days, minpartic
     listcellnucabundances = []
     print('Reading trajectory abundances...')
     timestart = time.perf_counter()
-    trajnucabundworker = partial(get_trajectory_time_nuc_abund, t_model_s=t_model_s)
+    trajnucabundworker = partial(get_trajectory_nuc_abund, t_model_s=t_model_s)
 
     if at.num_processes > 1:
         with multiprocessing.Pool(processes=at.num_processes) as pool:
@@ -261,7 +306,7 @@ def add_abundancecontributions(gridcontribpath, dfmodel, t_model_days, minpartic
     dfmodel.fillna(0., inplace=True)
     print(f' took {time.perf_counter() - timestart:.1f} seconds')
 
-    return dfmodel, dfelabundances
+    return dfmodel, dfelabundances, dfcontribs
 
 
 def addargs(parser):