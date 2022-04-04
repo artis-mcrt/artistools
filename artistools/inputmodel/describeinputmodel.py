53019ec3c933ccaa79a79f5c3e09636e488c7935
Luke Shingles <luke.shingles@gmail.com>
2022-04-04 08:17:24 +0100

    Update describeinputmodel.py

--- artistools/inputmodel/describeinputmodel.py
+++ artistools/inputmodel/describeinputmodel.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
-# import math
+import math
 # import os.path
 
 import numpy as np
@@ -18,6 +18,9 @@ def addargs(parser):
 
     parser.add_argument('-cell', '-mgi', default=None,
                         help='Focus on particular cell number (0-indexed)')
+
+    parser.add_argument('--noabund', action='store_true',
+                        help='Give total masses only, no nuclear or elemental abundances')
 
     parser.add_argument('--getabundances', action='store_true',
                         help='Get elemental abundance masses')
@@ -42,10 +45,14 @@ def main(args=None, argsraw=None, **kwargs):
 
     if 'pos_x_min' in dfmodel.columns:
         nonemptycells = sum(dfmodel['rho'] > 0.)
-        print(f'Model contains {len(dfmodel)} Cartesian grid cells ({nonemptycells} nonempty) with vmax = {vmax} km/s')
+        print(f'Model contains {len(dfmodel)} Cartesian grid cells ({nonemptycells} nonempty) with '
+              f'vmax = {vmax} km/s ({vmax / 299792.458:.2f} * c)')
+        corner_vmax = math.sqrt(3 * vmax ** 2)
+        print(f'  corner vmax: {corner_vmax:.2e} cm/s ({corner_vmax / 299792.458:.2f} * c)')
     else:
         vmax = dfmodel['velocity_outer'].max()
-        print(f'Model contains {len(dfmodel)} 1D spherical shells with vmax = {vmax} km/s')
+        print(f'Model contains {len(dfmodel)} 1D spherical shells with vmax = {vmax} km/s '
+              f'({vmax / 299792.458:.2f} * c)')
 
     if args.cell is not None:
         mgi = int(args.cell)
@@ -82,19 +89,20 @@ def main(args=None, argsraw=None, **kwargs):
 
     print(f'M_{"tot_iso":9s} {mass_msun_isotopes:8.5f} MSun ({mass_msun_isotopes / mass_msun_rho * 100:6.2f}% of M_tot_rho, but can be < 100% if stable isotopes not tracked)')
 
-    for species, mass_g in sorted(speciesmasses.items(), key=lambda x: (get_atomic_number(x[0]), x[0])):
-        species_mass_msun = mass_g / 1.989e33
-        massfrac = species_mass_msun / mass_msun_rho
-        strcomment = ''
-        if species.endswith('-isosum') and args.getabundances:
-            elsymb = species.replace('-isosum', '')
-            elem_mass = speciesmasses.get(elsymb, 0.)
-            if elem_mass > 0.:
-                strcomment += f' ({mass_g / elem_mass * 100:6.2f}% of {elsymb} element mass)'
-            if mass_g > elem_mass:
-                strcomment += ' ERROR! isotope sum is greater than element abundance'
+    if not args.noabund:
+        for species, mass_g in sorted(speciesmasses.items(), key=lambda x: (get_atomic_number(x[0]), x[0])):
+            species_mass_msun = mass_g / 1.989e33
+            massfrac = species_mass_msun / mass_msun_rho
+            strcomment = ''
+            if species.endswith('-isosum') and args.getabundances:
+                elsymb = species.replace('-isosum', '')
+                elem_mass = speciesmasses.get(elsymb, 0.)
+                if elem_mass > 0.:
+                    strcomment += f' ({mass_g / elem_mass * 100:6.2f}% of {elsymb} element mass)'
+                if mass_g > elem_mass:
+                    strcomment += ' ERROR! isotope sum is greater than element abundance'
 
-        print(f'{species:9s} {species_mass_msun:.3e} Msun    massfrac {massfrac:.3e}{strcomment}')
+            print(f'{species:9s} {species_mass_msun:.3e} Msun    massfrac {massfrac:.3e}{strcomment}')
 
 
 if __name__ == "__main__":
