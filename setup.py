72a4225cf355467c8384d291566d723aacf1d600
Luke Shingles <luke.shingles@gmail.com>
2022-04-07 09:46:36 +0100

    Add psutil to build requirements

--- setup.py
+++ setup.py
@@ -14,8 +14,8 @@ from artistools.commands import console_scripts, completioncommands
 # autoload -U bashcompinit
 # bashcompinit
 # source artistoolscompletions.sh
-with open('artistoolscompletions.sh', 'w') as f:
-    f.write('\n'.join(completioncommands))
+with open("artistoolscompletions.sh", "w") as f:
+    f.write("\n".join(completioncommands))
 
 
 setup(
@@ -28,15 +28,16 @@ setup(
     url="https://www.github.com/artis-mcrt/artistools/",
     license="MIT",
     description="Plotting and analysis tools for the ARTIS 3D supernova radiative transfer code.",
-    long_description=(Path(__file__).absolute().parent / "README.md").open('rt').read(),
-    long_description_content_type='text/markdown',
-    install_requires=(Path(__file__).absolute().parent / "requirements.txt").open('rt').read().splitlines(),
+    long_description=(Path(__file__).absolute().parent / "README.md").open("rt").read(),
+    long_description_content_type="text/markdown",
+    install_requires=(Path(__file__).absolute().parent / "requirements.txt").open("rt").read().splitlines(),
     entry_points={
-        'console_scripts': console_scripts,
+        "console_scripts": console_scripts,
     },
-    scripts=['artistoolscompletions.sh'],
-    python_requires='>==3.6',
+    scripts=["artistoolscompletions.sh"],
+    python_requires=">==3.6",
     # test_suite='tests',
-    setup_requires=["setuptools>=45", "wheel", "setuptools_scm[toml]>=6.2"],
-    tests_require=['pytest', 'pytest-runner', 'pytest-cov'],
-    include_package_data=True)
+    setup_requires=["psutil>=5.9.0", "setuptools>=45", "setuptools_scm[toml]>=6.2", "wheel"],
+    tests_require=["pytest", "pytest-runner", "pytest-cov"],
+    include_package_data=True,
+)
