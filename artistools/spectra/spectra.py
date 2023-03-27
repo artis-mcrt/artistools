60aa19ac170b18728b917c31c05196648f6e3feb
Luke Shingles <luke.shingles@gmail.com>
2023-03-27 16:36:24 +0100

    Update spectra.py

--- artistools/spectra/spectra.py
+++ artistools/spectra/spectra.py
@@ -173,9 +173,7 @@ def get_from_packets(
 
     dfpackets = dfpackets.filter((float(nu_min) <= pl.col("nu_rf")) & (pl.col("nu_rf") <= float(nu_max)))
     if not use_escapetime:
-        dfpackets = dfpackets.filter(
-            (float(timelow / 86400.0) <= pl.col("t_arrive_d")) & (pl.col("t_arrive_d") <= float(timehigh / 86400.0))
-        )
+        dfpackets = dfpackets.filter((timelowdays <= pl.col("t_arrive_d")) & (pl.col("t_arrive_d") <= timehighdays))
     else:
         dfpackets = dfpackets.filter(
             (timelow <= (pl.col("escape_time") * escapesurfacegamma))
