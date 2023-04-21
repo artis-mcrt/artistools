4b8a2c9894e1f167e3786118fb545d071a2238ab
Luke Shingles <luke.shingles@gmail.com>
2023-04-21 20:26:23 +0100

    Update lightcurve.py

--- artistools/lightcurve/lightcurve.py
+++ artistools/lightcurve/lightcurve.py
@@ -107,6 +107,7 @@ def get_from_packets(
             getcols.append("phibin")
         else:
             getcols.append("dirbin")
+
     dfpackets = dfpackets.select(getcols).collect(streaming=True).lazy()
 
     lcdata = {}
