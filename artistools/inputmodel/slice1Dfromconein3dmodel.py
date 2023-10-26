73d6528cdc96b413b05f7dddc825b0538d80558e
Christine <ccollins22@qub.ac.uk>
2023-10-26 10:00:29 +0200

    Update slice1Dfromconein3dmodel.py

--- artistools/inputmodel/slice1Dfromconein3dmodel.py
+++ artistools/inputmodel/slice1Dfromconein3dmodel.py
@@ -19,31 +19,29 @@ def make_cone(args):
     theta = np.radians([angle_of_cone / 2])  # angle between line of sight and edge is half angle of cone
 
     # merge_dfs, args.t_model, args.vmax = at.inputmodel.get_modeldata_tuple(args.modelpath[0], dimensions=3, get_elemabundances=True)
-    merge_dfs = at.inputmodel.get_3d_model_data_merged_model_and_abundances_minimal(args)
+    # merge_dfs = at.inputmodel.get_3d_model_data_merged_model_and_abundances_minimal(args)  ## only works with old 3D format
+    dfmodel, modelmeta = at.get_modeldata(modelpath=args.modelpath[0], get_elemabundances=True)
+    args.t_model = modelmeta["t_model_init_days"]
 
     if args.positive_axis:
-        cone = merge_dfs.loc[
-            merge_dfs[f"cellpos_in[{args.sliceaxis}]"]
+        print("using positive axis")
+        cone = dfmodel.loc[
+            dfmodel[f"pos_{args.sliceaxis}_min"]
             >= 1
             / (np.tan(theta))
-            * np.sqrt(
-                (merge_dfs[f"cellpos_in[{args.other_axis2}]"]) ** 2
-                + (merge_dfs[f"cellpos_in[{args.other_axis1}]"]) ** 2
-            )
+            * np.sqrt((dfmodel[f"pos_{args.other_axis2}_min"]) ** 2 + (dfmodel[f"pos_{args.other_axis1}_min"]) ** 2)
         ]  # positive axis
     else:
-        cone = merge_dfs.loc[
-            merge_dfs[f"cellpos_in[{args.sliceaxis}]"]
+        print("using negative axis")
+        cone = dfmodel.loc[
+            dfmodel[f"pos_{args.sliceaxis}_min"]
             <= -1
             / (np.tan(theta))
-            * np.sqrt(
-                (merge_dfs[f"cellpos_in[{args.other_axis2}]"]) ** 2
-                + (merge_dfs[f"cellpos_in[{args.other_axis1}]"]) ** 2
-            )
+            * np.sqrt((dfmodel[f"pos_{args.other_axis2}_min"]) ** 2 + (dfmodel[f"pos_{args.other_axis1}_min"]) ** 2)
         ]  # negative axis
     # print(cone.loc[:, :[f'pos_{slice_on_axis}']])
 
-    del merge_dfs  # merge_dfs not needed anymore so free memory
+    del dfmodel  # merge_dfs not needed anymore so free memory
     gc.collect()
 
     return cone
@@ -80,33 +78,33 @@ def make_1D_profile(args):
     if args.makefromcone:
         cone = make_cone(args)
 
-        slice1D = cone.groupby([f"cellpos_in[{args.sliceaxis}]"], as_index=False).mean()
+        slice1D = cone.groupby([f"pos_{args.sliceaxis}_min"], as_index=False).mean()
         # where more than 1 X value, average rows eg. (1,0,0) (1,1,0) (1,1,1)
 
     else:  # make from along chosen axis
         slice1D = get_profile_along_axis(args)
 
-    slice1D[f"cellpos_in[{args.sliceaxis}]"] = slice1D[f"cellpos_in[{args.sliceaxis}]"].apply(
+    slice1D[f"pos_{args.sliceaxis}_min"] = slice1D[f"pos_{args.sliceaxis}_min"].apply(
         lambda x: x / args.t_model * (u.cm / u.day).to("km/s")
     )  # Convert positions to velocities
-    slice1D = slice1D.rename(columns={f"cellpos_in[{args.sliceaxis}]": "vout_kmps"})
+    slice1D = slice1D.rename(columns={f"pos_{args.sliceaxis}_min": "vel_r_max_kmps"})
     # Convert position to velocity
 
     slice1D = slice1D.drop(
-        ["inputcellid", f"cellpos_in[{args.other_axis1}]", f"cellpos_in[{args.other_axis2}]"], axis=1
+        ["inputcellid", f"pos_{args.other_axis1}_min", f"pos_{args.other_axis2}_min"], axis=1
     )  # Remove columns we don't need
 
-    slice1D["rho_model"] = slice1D["rho_model"].apply(lambda x: np.log10(x) if x != 0 else -100)
+    slice1D["rho"] = slice1D["rho"].apply(lambda x: np.log10(x) if x != 0 else -100)
     # slice1D = slice1D[slice1D['rho_model'] != -100]  # Remove empty cells
     # TODO: fix this, -100 probably breaks things if it's not one of the outer cells that gets chopped
-    slice1D = slice1D.rename(columns={"rho_model": "log_rho"})
+    slice1D = slice1D.rename(columns={"rho": "logrho"})
 
     slice1D.index += 1
 
     if not args.positive_axis:
         # Invert rows and *velocity by -1 to make velocities positive for slice on negative axis
         slice1D.iloc[:] = slice1D.iloc[::-1].to_numpy()
-        slice1D["vout_kmps"] = slice1D["vout_kmps"].apply(lambda x: x * -1)
+        slice1D["vel_r_max_kmps"] = slice1D["vel_r_max_kmps"].apply(lambda x: x * -1)
 
     # with pd.option_context('display.max_rows', None, 'display.max_columns', None):
     #     print(slice1D)
@@ -117,7 +115,15 @@ def make_1D_profile(args):
 
 def make_1D_model_files(args):
     slice1D = make_1D_profile(args)
-    query_abundances_positions = slice1D.columns.str.startswith("X_")
+
+    # query_abundances_positions = slice1D.columns.str.startswith("X_")
+    query_abundances_positions = np.array(
+        [
+            (column.startswith("X_") and not (any(i.isdigit() for i in column))) and (len(column) < 5)
+            for column in slice1D.columns
+        ]
+    )
+
     model_df = slice1D.loc[:, ~query_abundances_positions]
     abundances_df = slice1D.loc[:, query_abundances_positions]
 
@@ -125,15 +131,23 @@ def make_1D_model_files(args):
     #     print(model_df)
 
     # print(modelpath)
-    model_df = model_df.round(decimals=5)  # model files seem to be to 5 sf
-    model_df.to_csv(args.modelpath[0] / "model_1D.txt", sep=" ", header=False)  # write model.txt
+    # model_df = model_df.round(decimals=5)  # model files seem to be to 5 sf
+    # model_df.to_csv(args.modelpath[0] / "model_1D.txt", sep=" ", header=False)  # write model.txt
 
-    abundances_df.to_csv(args.modelpath[0] / "abundances_1D.txt", sep=" ", header=False)  # write abundances.txt
+    npts_model = len(model_df)
+    inputcellid = np.arange(1, npts_model + 1)
+    model_df["inputcellid"] = inputcellid
+    abundances_df["inputcellid"] = inputcellid
 
-    with Path(args.modelpath[0], "model_1D.txt").open("r+") as f:  # add number of cells and tmodel to start of file
-        content = f.read()
-        f.seek(0, 0)
-        f.write(f"{model_df.shape[0]}\n{args.t_model}".rstrip("\r\n") + "\n" + content)
+    at.inputmodel.save_modeldata(dfmodel=model_df, t_model_init_days=args.t_model, filename=Path(".", "model_1D.txt"))
+
+    # abundances_df.to_csv(args.modelpath[0] / "abundances_1D.txt", sep=" ", header=False)  # write abundances.txt
+    at.inputmodel.save_initelemabundances(abundances_df, Path(".", "abundances_1D.txt"))
+
+    # with Path(args.modelpath[0], "model_1D.txt").open("r+") as f:  # add number of cells and tmodel to start of file
+    #     content = f.read()
+    #     f.seek(0, 0)
+    #     f.write(f"{model_df.shape[0]}\n{args.t_model}".rstrip("\r\n") + "\n" + content)
 
     print("Saved abundances_1D.txt and model_1D.txt")
 
@@ -218,6 +232,8 @@ def main(args: argparse.Namespace | None = None, argsraw: t.Sequence[str] | None
 
     if not args.modelpath:
         args.modelpath = [Path()]
+
+    print(f"making model from slice around {args.sliceaxis} axis")
 
     axes = ["x", "y", "z"]
     args.other_axis1 = next(ax for ax in axes if ax != args.sliceaxis)
