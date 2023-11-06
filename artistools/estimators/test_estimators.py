d4b4e43fdb3866d4f20fa0f4714b84a8d2fe4fe1
Luke Shingles <luke.shingles@gmail.com>
2023-11-06 10:24:00 +0000

    Add full test of estimator plotting

--- artistools/estimators/test_estimators.py
+++ artistools/estimators/test_estimators.py
@@ -10,17 +10,16 @@ outputpath = at.get_config()["path_testoutput"]
 
 plotlist = [
     [["initabundances", ["Fe", "Ni_stable", "Ni_56"]]],
-    ["heating_dep", "heating_coll", "heating_bf", "heating_ff", ["_yscale", "linear"]],
-    ["cooling_adiabatic", "cooling_coll", "cooling_fb", "cooling_ff", ["_yscale", "linear"]],
     ["nne"],
     ["TR", ["_yscale", "linear"], ["_ymin", 1000], ["_ymax", 22000]],
     ["Te"],
     [["averageionisation", ["Fe", "Ni"]]],
-    [["averageexcitation", ["Fe II", "Fe III"]]],
-    [["populations", ["Fe I", "Fe II", "Fe III", "Fe IV", "Fe V", "Fe VI", "Fe VII", "Fe VIII"]]],
-    [["populations", ["Co I", "Co II", "Co III", "Co IV", "Co V", "Co VI", "Co VII"]]],
-    [["populations", ["Ni I", "Ni II", "Ni III", "Ni IV", "Ni V", "Ni VI", "Ni VII"]]],
+    [["averageexcitation", ["Fe II"]]],
+    [["populations", ["Fe I", "Fe II", "Fe III", "Fe IV", "Fe V"]]],
+    [["populations", ["Co II", "Co III", "Co IV"]]],
     [["gamma_NT", ["Fe I", "Fe II", "Fe III", "Fe IV"]]],
+    ["heating_dep", "heating_coll", "heating_bf", "heating_ff", ["_yscale", "linear"]],
+    ["cooling_adiabatic", "cooling_coll", "cooling_fb", "cooling_ff", ["_yscale", "linear"]],
 ]
 
 
@@ -30,15 +29,50 @@ def test_estimator_snapshot(mockplot) -> None:
     xarr = [0.0, 4000.0]
     for x in mockplot.call_args_list:
         assert xarr == x[0][1]
-    yvals = [callargs[0][2] for callargs in mockplot.call_args_list]
-    init_fe = [0.10000000149011612, 0.1000000014901161]
-    assert np.allclose(init_fe, yvals[0], rtol=0.001)
-    init_nistable = [0, 0.0]
-    assert np.allclose(init_nistable, yvals[1], rtol=0.001)
-    init_ni56 = [0.8999999761581421, 0.8999999761581421]
-    assert np.allclose(init_ni56, yvals[2], rtol=0.001)
 
-    print("\n".join(str(x) for x in yvals))
+    # order of keys is important
+    expectedvals = {
+        "init_fe": 0.10000000149011612,
+        "init_nistable": 0.0,
+        "init_ni56": 0.8999999761581421,
+        "nne": 794211.0,
+        "TR": 6932.45,
+        "Te": 5776.620000000001,
+        "averageionisation_Fe": 1.9453616269532485,
+        "averageionisation_Ni": 1.970637712188408,
+        "averageexcitation_FeII": 0.19701832980731157,
+        "populations_FeI": 4.801001667392128e-05,
+        "populations_FeII": 0.350781150587666,
+        "populations_FeIII": 0.3951266859004141,
+        "populations_FeIV": 0.21184950941623004,
+        "populations_FeV": 0.042194644079016,
+        "populations_CoII": 0.10471832570699871,
+        "populations_CoIII": 0.476333358337709,
+        "populations_CoIV": 0.41894831595529214,
+        "gamma_NT_FeI": 7.571e-06,
+        "gamma_NT_FeII": 3.711e-06,
+        "gamma_NT_FeIII": 2.762e-06,
+        "gamma_NT_FeIV": 1.702e-06,
+        "heating_dep": 6.56117e-10,
+        "heating_coll": 2.37823e-09,
+        "heating_bf": 1.27067e-13,
+        "heating_ff": 1.86474e-16,
+        "cooling_adiabatic": 9.72392e-13,
+        "cooling_coll": 3.02786e-09,
+        "cooling_fb": 4.82714e-12,
+        "cooling_ff": 1.62999e-13,
+    }
+    assert len(expectedvals) == len(mockplot.call_args_list)
+    yvals = {varname: callargs[0][2] for varname, callargs in zip(expectedvals.keys(), mockplot.call_args_list)}
+
+    print({key: yarr[1] for key, yarr in yvals.items()})
+
+    for varname, expectedval in expectedvals.items():
+        assert np.allclose([expectedval, expectedval], yvals[varname], rtol=0.001), (
+            varname,
+            expectedval,
+            yvals[varname][1],
+        )
 
 
 @mock.patch.object(matplotlib.axes.Axes, "plot", side_effect=matplotlib.axes.Axes.plot, autospec=True)
