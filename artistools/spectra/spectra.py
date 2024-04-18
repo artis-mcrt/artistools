ab440b822c2b07508218551858c2e45ca6d3c63c
Luke Shingles <luke.shingles@gmail.com>
2024-04-18 10:44:25 +0100

    Update spectra.py

--- artistools/spectra/spectra.py
+++ artistools/spectra/spectra.py
@@ -271,7 +271,7 @@ def get_from_packets(
         elif average_over_phi:
             assert not average_over_theta
             solidanglefactor = ncosthetabins
-            pldfpackets_dirbin_lazy = dfpackets.filter(pl.col("costhetabin") * 10 == dirbin)
+            pldfpackets_dirbin_lazy = dfpackets.filter(pl.col("costhetabin") * nphibins == dirbin)
         elif average_over_theta:
             solidanglefactor = nphibins
             pldfpackets_dirbin_lazy = dfpackets.filter(pl.col("phibin") == dirbin)
