984e07d6d5019a9b20de5dfdaad6ceebcdc0e07f
Luke Shingles <luke.shingles@gmail.com>
2024-06-05 10:49:07 +0100

    Improve type hints

--- artistools/inputmodel/rprocess_from_trajectory.py
+++ artistools/inputmodel/rprocess_from_trajectory.py
@@ -65,7 +65,7 @@ def get_dfelemabund_from_dfmodel(dfmodel: pl.DataFrame, dfnucabundances: pl.Data
     # ensure cells with no traj contributions are included
     dfelabundances = (
         pl.DataFrame(pl.Series(name="inputcellid", values=dfmodel["inputcellid"], dtype=pl.Int32))
-        .join(dfelabundances_partial, how="left", left_on="inputcellid", right_on="inputcellid", coalesce=True)
+        .join(dfelabundances_partial, how="left", on="inputcellid", coalesce=True)
         .fill_null(0.0)
     )
 
@@ -481,9 +481,7 @@ def add_abundancecontributions(
 
     timestart = time.perf_counter()
     print("Merging isotopic abundances into dfmodel...", end="", flush=True)
-    dfmodel = dfmodel.join(
-        dfnucabundances, how="left", left_on="inputcellid", right_on="inputcellid", coalesce=True
-    ).fill_null(0)
+    dfmodel = dfmodel.join(dfnucabundances, how="left", on="inputcellid", coalesce=True).fill_null(0)
     print(f" took {time.perf_counter() - timestart:.1f} seconds")
 
     return dfmodel, dfelabundances, dfcontribs
