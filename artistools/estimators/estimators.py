4afe5054dc6158c8c01f03de7ca48ccba947851f
Luke Shingles <luke.shingles@gmail.com>
2024-06-17 00:18:53 +0100

    Update estimators.py

--- artistools/estimators/estimators.py
+++ artistools/estimators/estimators.py
@@ -253,7 +253,11 @@ def get_rankbatch_parquetfile(
 
         pldf_batch = None
         if use_rust:
-            pldf_batch = at.rustext.estimparse(str(folderpath), min(batch_mpiranks), max(batch_mpiranks))
+            pldf_batch = at.rustext.estimparse(str(folderpath), min(batch_mpiranks), max(batch_mpiranks)).with_columns(
+                modelgridindex=pl.col("modelgridindex").cast(pl.Int32),
+                timestep=pl.col("timestep").cast(pl.Int32),
+                titerations=pl.col("titerations").cast(pl.Int32),
+            )
         else:
             print(
                 f"  reading {len(estfilepaths)} estimator files from {folderpath.relative_to(Path(folderpath).parent)}...",
