04bc479d20258100077d4b8f39f496bdfd3dc482
Luke Shingles <luke.shingles@gmail.com>
2024-07-01 00:02:44 +0100

    Update test_atomic.py

--- artistools/atomic/test_atomic.py
+++ artistools/atomic/test_atomic.py
@@ -11,7 +11,9 @@ outputpath = Path(at.get_config()["path_testoutput"])
 
 @pytest.mark.benchmark()
 def test_get_levels() -> None:
-    at.atomic.get_levels(modelpath, get_transitions=True, get_photoionisations=True)
+    dflevels = at.atomic.get_levels(modelpath, get_transitions=True, get_photoionisations=True)
+    assert len(dflevels) == 12
+    assert len(dflevels.iloc[0]) == 6
 
 
 @pytest.mark.benchmark()
