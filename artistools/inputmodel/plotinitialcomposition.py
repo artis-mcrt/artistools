1b34d0a86433d9d712da87142a0546f7c44f1558
Luke Shingles <luke.shingles@gmail.com>
2024-10-21 12:40:30 +0100

    Attempt to fix shutil race condition

--- artistools/inputmodel/plotinitialcomposition.py
+++ artistools/inputmodel/plotinitialcomposition.py
@@ -257,7 +257,8 @@ def plot_most_abundant(modelpath, args: argparse.Namespace):
 def make_3d_plot(modelpath, args: argparse.Namespace) -> None:
     import pyvista as pv
 
-    pv.set_plot_theme("document")  # set white background # pyright: ignore[reportCallIssue]
+    # set white background
+    pv.set_plot_theme("document")  # type: ignore[attr-defined] # pyright: ignore[reportPrivateImportUsage]
 
     get_elemabundances = False
     # choose what surface will be coloured by
@@ -302,7 +303,7 @@ def make_3d_plot(modelpath, args: argparse.Namespace) -> None:
 
     x, y, z = np.meshgrid(xgrid, xgrid, xgrid)
 
-    mesh: t.Any = pv.StructuredGrid(x, y, z)
+    mesh: t.Any = pv.StructuredGrid(x, y, z)  # type: ignore[attr-defined] # pyright: ignore[reportCallIssue]
     print(mesh)  # tells you the properties of the mesh
 
     mesh[coloursurfaceby] = surfacecolorscale.ravel(order="F")  # add data to the mesh
@@ -328,7 +329,7 @@ def make_3d_plot(modelpath, args: argparse.Namespace) -> None:
         "label_font_size": 22,
     }
 
-    plotter = pv.Plotter()  # pyright: ignore[reportCallIssue]
+    plotter = pv.Plotter()  # type: ignore[attr-defined] # pyright: ignore[reportCallIssue]
     # plotter.add_mesh(mesh.outline(), color="k")
     plotcoloropacity = [0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]  # some choices: 'linear' 'sigmoid'
     # plotter.set_scale(0.95, 0.95, 0.95) # adjusts fig resolution
@@ -344,7 +345,7 @@ def make_3d_plot(modelpath, args: argparse.Namespace) -> None:
         font_size=28,
         bold=False,
     )
-    plotter.add_mesh(surf, opacity=plotcoloropacity, scalar_bar_args=sargs, cmap="coolwarm_r")
+    plotter.add_mesh(surf, opacity=plotcoloropacity, scalar_bar_args=sargs, cmap="coolwarm_r")  # pyright: ignore[reportArgumentType]
     # plotter.add_mesh(surf, opacity=plotcoloropacity, use_transparency=True, cmap='coolwarm_r') #magma
 
     # plotter.remove_scalar_bar() # removes colorbar
