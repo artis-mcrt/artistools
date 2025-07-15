ed003526e6ab027c1b763ca617348ac39c16060a
gleck97 <gerritleck@web.de>
2025-07-15 16:56:23 +0200

    correct return type error

--- artistools/inputmodel/from_e2e_model.py
+++ artistools/inputmodel/from_e2e_model.py
@@ -456,7 +456,7 @@ def create_ARTIS_modelfile(
     at.inputmodel.save_modeldata(dfmodel=dfmodel, modelmeta=modelmeta, outpath=outputpath)
 
 
-def get_old_cell_indices(red_fact: int, new_r: int, new_z: int, N_cell_r_old: int) -> list[int]:
+def get_old_cell_indices(red_fact: int, new_r: int, new_z: int, N_cell_r_old: int) -> npt.NDArray[np.integer]:
     # function to get old grid indices for a given new grid index
     old_r_indices = np.arange(red_fact * (new_r - 1) + 1, red_fact * new_r + 1)
     old_z_indices = np.arange(red_fact * (new_z - 1), red_fact * new_z) * N_cell_r_old
