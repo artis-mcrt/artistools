e904c3e78504b9c749096534b80acb8b4b4fb78b
Luke Shingles <luke.shingles@gmail.com>
2025-08-28 13:48:09 +0100

    Update test_estimators.py

--- artistools/estimators/test_estimators.py
+++ artistools/estimators/test_estimators.py
@@ -184,6 +184,7 @@ def test_estimator_snapshot_classic_3d(mockplot: t.Any) -> None:
         argsraw=[],
         modelpath=modelpath_classic_3d,
         plotlist=plotlist,
+        markersonly=True,
         outputfile=outputpath / "test_estimator_snapshot_classic_3d",
         timedays=4,
     )
