34f75ba3d33935d91559a18593bd5571274b20e7
Luke Shingles <luke.shingles@gmail.com>
2025-09-04 11:55:27 +0100

    estimators: simplify and allow plotting with no estimator data (snapshot and input.txt only)

--- artistools/estimators/estimators.py
+++ artistools/estimators/estimators.py
@@ -277,7 +277,9 @@ def scan_estimators(
         # for older files with no deposition data, take heating part of deposition and heating fraction
         pldflazy = pldflazy.with_columns(total_dep=pl.col("heating_dep") / pl.col("heating_heating_dep/total_dep"))
 
-    pldflazy = pldflazy.with_columns(nntot=pl.sum_horizontal(cs.starts_with("nnelement_"))).fill_null(0)
+    if any(col.startswith("nnelement_") for col in colnames):
+        pldflazy = pldflazy.with_columns(nntot=pl.sum_horizontal(cs.starts_with("nnelement_"))).fill_null(0)
+
     if join_modeldata:
         pldflazy, _ = join_cell_modeldata(estimators=pldflazy, modelpath=modelpath, verbose=verbose)
 
