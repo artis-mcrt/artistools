c548ff6335354b0efbcfc636cb0c6b5b08ed8fa3
Luke Shingles <luke.shingles@gmail.com>
2025-10-10 12:08:23 +0100

    Update packets.py

--- artistools/packets/packets.py
+++ artistools/packets/packets.py
@@ -537,7 +537,7 @@ def get_rankbatch_parquetfile(
         tempparquetfilepath = Path(
             tempfile.mkstemp(dir=packetdir, prefix=f"{parquetfilename}.partial", suffix=".tmp")[1]
         )
-        pldf_batch.lazy().sink_parquet(tempparquetfilepath, compression="zstd", statistics="full", compression_level=8)
+        pldf_batch.lazy().sink_parquet(tempparquetfilepath, compression="zstd", statistics="full", compression_level=12)
         if parquetfilepath.exists():
             tempparquetfilepath.unlink()
         else:
