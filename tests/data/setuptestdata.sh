8ceab7a2fb9cd524d926a0f158aef423ba8079e5
Luke Shingles <luke.shingles@gmail.com>
2026-03-17 21:59:22 +0000

    Update setuptestdata.sh

--- tests/data/setuptestdata.sh
+++ tests/data/setuptestdata.sh
@@ -15,10 +15,11 @@ tar -xf vspecpolmodel.tar.xz
 if [ ! -f test_classicmode_3d.tar.xz ]; then curl -O https://theory.gsi.de/~lshingle/artis_http_public/artistools/test_classicmode_3d.tar.xz; fi
 tar -xf test_classicmode_3d.tar.xz
 
-unxz testmodel/*.xz
 
 cp grid.out testmodel/
 
 xz -d testmodel/transitiondata.txt.xz
+
+unxz testmodel/*.xz
 
 set +x
