1810f53bfca5f075db99b8bd5a733baaaf4ab226
Luke Shingles <luke.shingles@gmail.com>
2026-06-05 12:30:19 +0100

    Move ejectaopacity script

--- artistools/commands.py
+++ artistools/commands.py
@@ -33,7 +33,7 @@ subcommandtree: CommandType = {
     "gsinetworkdecayproducts": ("gsinetwork.decayproducts", "main"),
     "describeinputmodel": ("inputmodel.describeinputmodel", "main"),
     "estimators": ("estimators.plotestimators", "main"),
-    "ejectaopacity": ("plotejectopacity", "main"),
+    "ejectaopacity": ("estimators.ejectaopacity", "main"),
     "exportmassfractions": ("estimators.exportmassfractions", "main"),
     "getpath": ("commands", "get_artistools_path"),
     "lc": ("lightcurve.plotlightcurve", "main"),
